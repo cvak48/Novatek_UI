0d1739f2863120db6a4c6dd3700360b67b1ba572
Dhaval Tripathi <dhavaltripathi29@gmail.com>
2021-04-28 22:11:04 +0530

    add layout folder and footer component

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NovaFooterComponent } from './nova-footer/nova-footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    NovaFooterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }

c15c42c371ed023e52e1be80f5ad10016186653e
Sina-pz <sp.zanoosi@gmail.com>
2021-05-11 15:22:07 -0400

    header updated

--- src/app/reusable/header/header.component.ts
+++ src/app/reusable/header/header.component.ts
@@ -1,3 +1,4 @@
+import { Notification, Person } from './../demo-page/demo-page.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  person: Person = mockAvatar();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+function mockAvatar(): Person {
+  const avatarProps = {
+    id: 1,
+    name: 'Alex Green',
+    imageUrl: 'https://tse1.mm.bing.net/th?id=OIP.E45HCyveqDL44p8lmvQL9AAAAA&pid=Api&P=0',
+    notification: {
+      number: 20,
+      content: '',
+      hasAttachment: false,
+    } as Notification,
+  } as Person;
+  return avatarProps;
 }

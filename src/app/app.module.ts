930444301125d4334a9ffe54b6697e691d58e551
Dhaval Tripathi <dhavaltripathi29@gmail.com>
2021-05-13 22:14:58 +0530

    update appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { LayoutComponentModule } from './layout/layout.module';
 import { ReusableModule } from './reusable/reusable.module';
 
 @NgModule({
@@ -17,7 +16,6 @@ import { ReusableModule } from './reusable/reusable.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    LayoutComponentModule,
     ReusableModule,
     MainModule
   ],

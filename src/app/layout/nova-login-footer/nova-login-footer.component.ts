bf18fc9c80e59c59321802eccc048359fd1c23fc
Dhaval Tripathi <dhavaltripathi29@gmail.com>
2021-05-20 21:05:59 +0530

    add footer

---
+++ src/app/layout/nova-login-footer/nova-login-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nova-login-footer',
+  templateUrl: './nova-login-footer.component.html',
+  styleUrls: ['./nova-login-footer.component.scss']
+})
+export class NovaLoginFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

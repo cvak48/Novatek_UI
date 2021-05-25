66cd879575500296ef6e02159e8ce1f302c33ca5
Sina-pz <sp.zanoosi@gmail.com>
2021-05-25 12:50:17 -0400

    added

--- src/app/main/components/demo-page/demo-page.component.ts
+++ src/app/main/components/demo-page/demo-page.component.ts
@@ -32,7 +32,7 @@ export enum NvComponent {
   styleUrls: ['./demo-page.component.scss']
 })
 export class DemoPageComponent implements OnInit {
-  public selectedItem: NvComponent = NvComponent.DropToUpload;
+  public selectedItem: NvComponent = NvComponent.DragDrop;
   public nvComponentType = NvComponent;
    // Second: Provide input data for your reusable component here if needed
    // Search

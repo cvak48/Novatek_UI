543850f45ec4d1becadb2c710a2804f8f41336a9
Sina-pz <sp.zanoosi@gmail.com>
2021-07-02 12:14:08 -0400

    search feature fixed

--- src/app/reusable/custom-datatable/custom-datatable.component.ts
+++ src/app/reusable/custom-datatable/custom-datatable.component.ts
@@ -1,8 +1,9 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import { FilterAllPipe } from './../pipes/filters/filterAll/filter-all.pipe';
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 // import {TableColumn} from "./TableColumn";
-import {MatSort, Sort} from "@angular/material/sort";
-import {MatTableDataSource} from "@angular/material/table";
-import {MatPaginator} from "@angular/material/paginator";
+import { MatSort, Sort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
 import { TableColumn } from './table-column.model';
 import { ThrowStmt } from '@angular/compiler';
 import { SelectionModel } from '@angular/cdk/collections';
@@ -16,36 +17,45 @@ export class CustomDatatableComponent implements OnInit, AfterViewInit {
 
   public tableDataSource = new MatTableDataSource([]);
   public displayedColumns: string[] = [];
-  
+
   @ViewChild(MatPaginator, { static: false })
   matPaginator!: MatPaginator;
   @ViewChild(MatSort, { static: true })
   matSort!: MatSort;
   totalRec = 26;
   currentRec = 5;
-  tabletestData: any[]= [];
+
   @Input() isPageable = false;
   @Input() isSortable = false;
   @Input() isFilterable = false;
   @Input() tableColumns: TableColumn[] = [];
-  searchableRefList: any[] = [ 'id' , 'name', 'data', 'email', 'status', 'checked', 'attachment'];
-  
+
+
   @Input() rowActionIcon: string = '';
   @Input() paginationSizes: number[] = [5, 10, 15];
   @Input() defaultPageSize = this.paginationSizes[1];
-  
+
   @Output() sort: EventEmitter<Sort> = new EventEmitter();
   @Output() rowAction: EventEmitter<any> = new EventEmitter<any>();
+  // search
+  tableTestData: any[] = mockSearchComponent().tableTestData;
+  isAdvance: boolean   = mockSearchComponent().isAdvance ;
+  hideMenu: boolean    = mockSearchComponent().hideMenu ;
+  searchableRefList: any[] = mockSearchComponent().searchableRefList ;
 
   // this property needs to have a setter, to dynamically get changes from parent component
   @Input() set tableData(data: any[]) {
+    if (data) {
+    this.tableTestData = data;
     this.setTableDataSource(data);
+    } else {
+      this.tableTestData = []
+    }
   }
 
   apiData: any[] = [];
   selection = new SelectionModel<any>(true, []);
-  constructor() {
-  }
+  constructor() {  }
 
   ngOnInit(): void {
     const columnNames = this.tableColumns.map((tableColumn: TableColumn) => tableColumn.name);
@@ -54,25 +64,27 @@ export class CustomDatatableComponent implements OnInit, AfterViewInit {
     } else {
       this.displayedColumns = columnNames;
     }
-   
   }
 
   // we need this, in order to make pagination work with *ngIf
   ngAfterViewInit(): void {
     this.tableDataSource.paginator = this.matPaginator;
-     
   }
 
 
   setTableDataSource(data: any) {
-  
-    if(data) {
-      console.log("Service data >> ",data);
-      this.tabletestData = data;
-    this.apiData = [...data];
-    this.tableDataSource = new MatTableDataSource(data);
-    this.tableDataSource.paginator = this.matPaginator;
-    this.tableDataSource.sort = this.matSort;
+    if (data) {
+      console.log("Service data >> ", data);
+      this.apiData = [...data];
+      this.tableDataSource = new MatTableDataSource(data);
+      this.tableDataSource.paginator = this.matPaginator;
+      this.tableDataSource.sort = this.matSort;
+    }
+  }
+  onItemsFilter(data: any): any {
+      if (data) {
+      console.log('onFilter >>'  + JSON.stringify(data));
+      this.setTableDataSource(data);
     }
   }
 
@@ -81,15 +93,10 @@ export class CustomDatatableComponent implements OnInit, AfterViewInit {
     this.tableDataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  onItemsFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.tableDataSource.filter = filterValue.trim().toLowerCase();
-  }
-
   sortTable(sortParameters: Sort): any {
     console.log("sort para > ", sortParameters);
     let abc;
-    for(let i =0; i< this.tableColumns.length; i++) {
+    for (let i = 0; i < this.tableColumns.length; i++) {
       if (this.tableColumns[i].name === sortParameters.active) {
         abc = this.tableColumns[i];
       }
@@ -102,8 +109,8 @@ export class CustomDatatableComponent implements OnInit, AfterViewInit {
       console.log("in find", column.name === sortParameters.active)
       return (column.name === sortParameters.active) ? column.dataKey.toString() : 'name'
     } );*/
-    console.log('column find',abc)
-    sortParameters.active = abc?.dataKey ?  abc?.dataKey : 'name'
+    console.log('column find', abc)
+    sortParameters.active = abc?.dataKey ? abc?.dataKey : 'name'
 
     // console.log("abc >> ",abc )
     // sortParameters.active = "amount";
@@ -116,21 +123,21 @@ export class CustomDatatableComponent implements OnInit, AfterViewInit {
   }
 
   onPageFired(event: any) {
-   if ((this.currentRec <= ((event.pageIndex+1) * event.pageSize)) && this.currentRec <= this.totalRec) {
-     for(let i=0; i<5 ; i++) {
-      this.currentRec +=1;
-      this.apiData.push({
-        id: i+5,
-        name: 'Vikash Gupta'+i,
-        date: "03-30-2005",
-        email: 'cikash@gmail.com',
-        status: 'Completed'
-      },
-     )
-     }
-     
-   this.setTableDataSource(this.apiData);
-   }
+    if ((this.currentRec <= ((event.pageIndex + 1) * event.pageSize)) && this.currentRec <= this.totalRec) {
+      for (let i = 0; i < 5; i++) {
+        this.currentRec += 1;
+        this.apiData.push({
+          id: i + 5,
+          name: 'Vikash Gupta' + i,
+          date: "03-30-2005",
+          email: 'cikash@gmail.com',
+          status: 'Completed'
+        },
+        )
+      }
+
+      this.setTableDataSource(this.apiData);
+    }
   }
 
   isAllSelected() {
@@ -140,14 +147,25 @@ export class CustomDatatableComponent implements OnInit, AfterViewInit {
   }
   masterToggle() {
     this.isAllSelected() ?
-        this.selection.clear() :
-        this.tableDataSource.data.forEach(row => this.selection.select(row));
+      this.selection.clear() :
+      this.tableDataSource.data.forEach(row => this.selection.select(row));
   }
-   
+
 
   logSelection(row: any) {
-   console.log(row)
-   console.log(!this.selection.isSelected(row))
+    console.log(row)
+    console.log(!this.selection.isSelected(row))
     //this.selection.selected.forEach(s => console.log(s.name));
   }
+}
+
+
+function mockSearchComponent() {
+ const inputData = {
+    tableTestData: [],
+    isAdvance: true,
+    hideMenu: true,
+    searchableRefList: ['name','date', 'email', 'status', 'checked'],
+  }
+  return inputData;
 }

83febb40a955e14853014fb8ed28b0d8b701cd8e
Sina-pz <sp.zanoosi@gmail.com>
2021-07-05 09:21:58 -0400

    style fixed but arrow

--- src/app/reusable/nv-dropdown/nv-dropdown.component.ts
+++ src/app/reusable/nv-dropdown/nv-dropdown.component.ts
@@ -6,9 +6,10 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./nv-dropdown.component.scss']
 })
 export class NvDropdownComponent implements OnInit {
+  //TODO:By clicking on main page the arrow should change
   @Input() items: string[] = ['item1', 'item2 which is longer', 'item3 item2 which is longer and longer', 'item4', 'item5', 'item6', 'item7'];
   showMenu!: boolean;
-  selectedItem: string = 'Select item';
+  selectedItem: string = 'Select dropdown item';
   selectedIndex!: number;
   isFirst: boolean = true;
   isArrowDown: boolean = true;

1820a908eda8fbd4d969b23f549fe6a036b8ba10
Sina-pz <sp.zanoosi@gmail.com>
2021-07-07 16:33:54 -0400

    not fixed

--- src/app/reusable/nv-data-picker/nv-data-picker.component.ts
+++ src/app/reusable/nv-data-picker/nv-data-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nv-data-picker',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nv-data-picker.component.scss']
 })
 export class NvDataPickerComponent implements OnInit {
+  @Input() isDateRangePicker = true;
 
   constructor() { }
 

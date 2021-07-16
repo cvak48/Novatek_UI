78fba3c248fe76157043acc2542e68bd2887fa05
Sina-pz <sp.zanoosi@gmail.com>
2021-07-16 09:07:36 -0400

    box-shadow fixed

--- src/app/reusable/nv-data-picker/nv-data-picker.component.ts
+++ src/app/reusable/nv-data-picker/nv-data-picker.component.ts
@@ -22,7 +22,7 @@ export const MY_FORMATS = {
     {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
     // {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
     {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
-    {provide:  MAT_DATE_LOCALE, useValue: 'es-AR'},
+    {provide:  MAT_DATE_LOCALE, useValue: 'de-LU'},
   ]
 })
 export class NvDataPickerComponent implements OnInit {

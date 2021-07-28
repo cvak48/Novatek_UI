f14c51013f141f2128ba4435479cd6429e61153e
Sina-pz <sp.zanoosi@gmail.com>
2021-07-28 16:30:32 -0400

    not completed format

--- src/app/reusable/nv-date-time-picker/nv-date-time-picker.component.ts
+++ src/app/reusable/nv-date-time-picker/nv-date-time-picker.component.ts
@@ -1,18 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+  /**
+  * large size field has both toggle and 12 format
+  * medium size is just 12 timeFormat without toggle
+  * small size is 24 timeFormat without toggle
+ */
+export interface TimePickerSetting {
+  isTimePicker: boolean,
+  hasToggle: boolean,
+  timeFormat: '12' | '24',
+  fieldSize: 'small' | 'medium' | 'large',
+}
 
 @Component({
   selector: 'app-nv-date-time-picker',
   templateUrl: './nv-date-time-picker.component.html',
   styleUrls: ['./nv-date-time-picker.component.scss']
 })
-export class NvDateTimePickerComponent implements OnInit {
+export class NvDateTimePickerComponent implements OnInit, TimePickerSetting {
+  @Input() set timePickerSetting(value: TimePickerSetting) {
+    if(value) {
+
+    } else {
+      
+    }
+
+  }
+
+  timePickerSettingDefault!: TimePickerSetting;
+  isTimePicker!: boolean;
+  hasToggle!: boolean;
+  timeFormat!: '12' | '24';
+  fieldSize!: 'small' | 'medium' | 'large';
+
   myDatePickerFrom: any;
   myDatePickerTo: any;
-  // myDatePicker: any;
+  myDatePicker: any;
 
   // myTimePicker: any;
 
   constructor() { }
+
 
   ngOnInit(): void {
   }
@@ -23,4 +50,13 @@ export class NvDateTimePickerComponent implements OnInit {
 
   }
 
+}
+
+function populateTimeSettingDefault(): TimePickerSetting {
+  const defaultSetting = {
+    isTimePicker: true,
+    hasToggle: false,
+    timeFormat: '12',
+    fieldSize: 'medium',
+  }
 }

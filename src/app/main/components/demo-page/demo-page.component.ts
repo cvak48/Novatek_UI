939f9f59196e59767e09b6490c7aafba6ab52f8a
Sina-pz <sp.zanoosi@gmail.com>
2021-07-30 13:17:03 -0400

    not fixed

--- src/app/main/components/demo-page/demo-page.component.ts
+++ src/app/main/components/demo-page/demo-page.component.ts
@@ -38,7 +38,7 @@ export enum NvComponent {
   styleUrls: ['./demo-page.component.scss']
 })
 export class DemoPageComponent implements OnInit {
-  public selectedItem: NvComponent = NvComponent.NvButton;
+  public selectedItem: NvComponent = NvComponent.NvDatePicker;
   public nvComponentType = NvComponent;
    // Second: Provide input data for your reusable component here if needed
    // Search

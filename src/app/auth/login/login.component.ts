c360fa2c7874117052b2d7a88754e2abeb2b66df
veeranjaneyuludavuluri <veeranji826@gmail.com>
2021-08-02 01:56:48 -0400

    login modifications

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -17,7 +17,7 @@ export class LoginComponent implements OnInit {
   ]
 
   public customLogoSize = {
-    width:'200',
+    width: '200',
     height: '50'
   }
 
@@ -31,8 +31,13 @@ export class LoginComponent implements OnInit {
   public Login: string = 'Login';
   public selectedNumber: number = 2;
   public stateTxt: string = 'Select Site';
-  public domainTxt:string = 'Select Domain'
-  
+  public domainTxt: string = 'Select Domain';
+  public hideLogin: boolean = false;
+  public btnText: string = 'Next';
+  public cardHeading: string = 'User Login';
+  public confirmation: boolean = false;
+  public passwordDetails: boolean = false;
+
   constructor() { }
 
   /**
@@ -44,17 +49,68 @@ export class LoginComponent implements OnInit {
   /**
   * showPassword vairable state to shows the password feild
   */
-  askPassword() {
-    this.showPassword = true;
+  btnClick(text: string) {
+    switch (text) {
+      case 'Next':
+        this.showPassword = true;
+        this.btnText = 'Login';
+        this.confirmation = false;
+        this.passwordDetails = true;
+        break;
+      case 'Reset':
+        this.recoveryConfirmation();
+        this.btnText = 'Resend';
+
+        break;
+      case 'Sent':
+        this.recoveryConfirmation();
+
+        break;
+      case 'Login':
+
+        break;
+        break;
+      case 'Resend':
+
+        break;
+      default:
+        this.cancleClick();
+        this.cardHeading = 'User Login';
+        break;
+    }
   }
 
-  // Auth the user
-  authenticateuser() {
-    
+  public forgotDetails(text: string) {
+    this.hideLogin = true;
+    switch (text) {
+      case 'password':
+        this.btnText = 'Reset';
+        this.cardHeading = 'Forgot Password';
+        this.showPassword = true;
+        break;
+
+      default:
+        this.btnText = 'Sent';
+        this.cardHeading = 'Forgot Username';
+        this.showPassword = true;
+        break;
+    }
   }
 
-  onStateSelect(e:any) {
+  public recoveryConfirmation() {
+    this.cardHeading = 'Confirmation';
+    this.confirmation = true;
+    this.btnText = 'Resend';
+    this.showPassword = false;
+  }
 
+  public cancleClick() {
+    this.showPassword = false;
+    this.hideLogin = false;
+    this.passwordDetails = false;
+    this.btnText = 'Next';
+    this.confirmation = false;
+    this.cardHeading = "User Login"
   }
 
 }

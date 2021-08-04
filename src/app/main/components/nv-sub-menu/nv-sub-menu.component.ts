32b48224fd518e87c0a6ed20414fd0a495436d85
Dhaval Tripathi <dhavaltripathi29@gmail.com>
2021-08-04 13:24:18 +0530

    sub header created

--- src/app/main/components/nv-sub-menu/nv-sub-menu.component.ts
+++ src/app/main/components/nv-sub-menu/nv-sub-menu.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nv-sub-menu.component.scss']
 })
 export class NvSubMenuComponent implements OnInit {
-
+  menuItems = ['menu item 1', 'menu item 2'];
+  siteItems = ['site item 1', 'site item 2'];
+  actionItems = ['action 1', 'action 2'];
   constructor() { }
 
   ngOnInit(): void {

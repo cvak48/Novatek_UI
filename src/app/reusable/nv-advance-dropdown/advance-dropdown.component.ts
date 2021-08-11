24502b9c7fa30b6b366aeba1da57f1f167751214
Sina-pz <sp.zanoosi@gmail.com>
2021-08-11 16:16:32 -0400

    remove functionality is not fixed

--- src/app/reusable/nv-advance-dropdown/advance-dropdown.component.ts
+++ src/app/reusable/nv-advance-dropdown/advance-dropdown.component.ts
@@ -1,5 +1,5 @@
 import { ArrowIcon } from './../../model/data-model';
-import { Observable } from 'rxjs';
+import { Observable, onErrorResumeNext, of, from } from 'rxjs';
 import { AdvanceFilterPipe } from '../pipes/filters/advance-filter/advance-filter.pipe';
 import { FilterAllPipe } from '../pipes/filters/filterAll/filter-all.pipe';
 import { Component, Input, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
@@ -26,33 +26,34 @@ export class AdvanceDropdownComponent implements OnInit {
   removable = true;
   addOnBlur = true;
   separatorKeysCodes: number[] = [ENTER, COMMA];
-  fruitCtrl = new FormControl();
-  filteredFruits!: Observable<string[]>;
-  fruits: string[] = ['Select'];
-  allFruits: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
-  @ViewChild('fruitInput') fruitInput!: ElementRef<HTMLInputElement>;
+  itemCtrl = new FormControl();
+  // menu
+  filteredItems!: Observable<string[]>;
+  // field
+  items: string[] = ['Multiple Select'];
+  referenceItems: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+  @ViewChild('itemInput') itemInput!: ElementRef<HTMLInputElement>;
   @ViewChild('auto') matAutocomplete!: MatAutocomplete;
   isArrowDown: boolean = true;
   readonly arrowIcons: ArrowIcon = {
     upward: '../../../assets/icons/ico.arrow.up.svg',
     downward: '../../../assets/icons/ico.arrow.down.svg'
   };
+  hasFruit = true;
   /**
    *
    */
   constructor() {
-    this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
+    this.filteredItems = this.itemCtrl.valueChanges.pipe(
       startWith(null),
       // The Array.slice() method returns a new array
-      map((fruit: string | null) => fruit ? this._filter(fruit) : this.allFruits.slice()));
+      map((item: string | null) => item ? this._filter(item) : this.referenceItems.slice()));
   }
   onBlur(): void {
     this.isArrowDown = true;
-    console.log('hi blur');
     if (this.matAutocomplete.isOpen) {
       this.isArrowDown = true;
-    } 
-    
+    }
   }
   onFieldClick(): void {
     if (this.matAutocomplete.isOpen) {
@@ -60,53 +61,71 @@ export class AdvanceDropdownComponent implements OnInit {
     } else {
       this.isArrowDown = true;
     }
-
   }
 
   add(event: MatChipInputEvent): void {
     // Add fruit only when MatAutocomplete is not open
     // To make sure this does not conflict with OptionSelected Event
     if (!this.matAutocomplete.isOpen) {
+
       const input = event.input;
       const value = event.value;
 
       // Add our fruit
       if ((value || '').trim()) {
-        this.fruits.push(value.trim());
+        this.items.push(value.trim());
       }
 
       // Reset the input value
       if (input) {
         input.value = '';
       }
-
-      this.fruitCtrl.setValue(null);
+      this.itemCtrl.setValue(null);
     }
+
   }
 
   remove(fruit: string): void {
-    const index = this.fruits.indexOf(fruit);
+    const index = this.items.indexOf(fruit);
+    // 
 
     if (index >= 0) {
-      this.fruits.splice(index, 1);
+      this.items.splice(index, 1);
+    }
+    if (this.items.length === 0) {
+      this.isArrowDown = false;
+      this.hasFruit = false;
     }
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.fruits.push(event.option.viewValue);
-    this.fruitInput.nativeElement.value = '';
-    this.fruitCtrl.setValue(null);
+    this.items.push(event.option.viewValue);
+    console.log(event.option.viewValue);
+    this.filteredItems.subscribe(
+      values => {
+        const newList = values.filter(item => item !== event.option.viewValue);
+        const obsList = of<string[]>(newList);
+        console.log(obsList);
+        
+        this.filteredItems = obsList;
+      }
+    );
+    this.itemInput.nativeElement.value = '';
+    this.itemCtrl.setValue(null);
     if (!this.isArrowDown) {
       this.isArrowDown = true;
+    }
+    if (this.items.length > 0) {
+      this.hasFruit = true;
     }
   }
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-    const filteredFruits = this.allFruits.filter(fruit => fruit.toLowerCase().indexOf(filterValue) === 0);
+    const filteredFruits = this.referenceItems.filter(fruit => fruit.toLowerCase().indexOf(filterValue) === 0);
     return filteredFruits;
   }
 
-  ngOnInit(): void {  }
+  ngOnInit(): void { }
 
 }

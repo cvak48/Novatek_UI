cba88e6082256f6cd6a5b0b7dec5d21104ca237e
Sina-pz <sp.zanoosi@gmail.com>
2021-08-30 09:50:20 -0400

    filter issue fixed

--- src/app/main/components/demo-page/demo-page.component.ts
+++ src/app/main/components/demo-page/demo-page.component.ts
@@ -27,7 +27,7 @@ export enum NvComponent {
   styleUrls: ['./demo-page.component.scss']
 })
 export class DemoPageComponent implements OnInit {
-  public selectedItem: NvComponent = NvComponent.StatusColorDirectives;
+  public selectedItem: NvComponent = NvComponent.Dropdown;
   public nvComponentType = NvComponent;
    // Second: Provide input data for your reusable component here if needed
    // Search

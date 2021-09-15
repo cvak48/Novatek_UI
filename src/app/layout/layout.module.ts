ec20e3d15e77cf4094324afeaaf3bb1ad45bba15
Dhaval Tripathi <dhavaltripathi29@gmail.com>
2021-09-15 12:35:15 +0530

    comments implemented

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,6 +3,7 @@ import { NvHeaderComponent } from './nv-header/nv-header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NvFooterComponent } from './nv-footer/nv-footer.component';
+import { NvSidenavComponent } from './nv-sidenav/nv-sidenav.component';
 import { MaterialModule } from '../material.module';
 import { NovaLoginFooterComponent } from './nova-login-footer/nova-login-footer.component';
 import { FormsModule } from '@angular/forms';
@@ -11,6 +12,7 @@ import { NvSubMenuComponent } from './nv-sub-menu/nv-sub-menu.component';
 
 const sharedComponents = [
   NvFooterComponent,
+  NvSidenavComponent,
   NvHeaderComponent,
   NovaLoginFooterComponent,
   NvSubMenuComponent

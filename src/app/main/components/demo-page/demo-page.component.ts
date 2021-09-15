c5a01dc1d776346e17e66a06d88fe6ff375ce3c4
Sina-pz <sp.zanoosi@gmail.com>
2021-09-15 15:55:10 -0400

    demo page done

--- src/app/main/components/demo-page/demo-page.component.ts
+++ src/app/main/components/demo-page/demo-page.component.ts
@@ -5,6 +5,7 @@ import { Notification, Person, DropdownFieldType, ButtonThemeColor, MenuExtensio
 export enum NvComponent {
   // First: Add the name of your reusable component to this enum
   Search,
+  NvPanel,
   MultiSelectDropdown,
   TreeViewDropdown,
   NvLogo,
@@ -30,7 +31,7 @@ export enum NvComponent {
   styleUrls: ['./demo-page.component.scss']
 })
 export class DemoPageComponent implements OnInit {
-  public selectedItem: NvComponent = NvComponent.NvTimePicker;
+  public selectedItem: NvComponent = NvComponent.NvPanel;
   public nvComponentType = NvComponent;
   // Second: Provide input data for your reusable component here if needed
   // Search
@@ -58,7 +59,6 @@ export class DemoPageComponent implements OnInit {
   loginFieldStatusType = FieldStatusType.Error;
   // checkbox
   checkBoxLabel = 'Im a new Checkbox';
-
   /**
    * timePicker
    * 12 or 24
@@ -66,8 +66,7 @@ export class DemoPageComponent implements OnInit {
   timeFormat: number = 24;
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {  }
   public onItemClick(selectedItem: NvComponent): void {
     this.selectedItem = selectedItem;
   }
@@ -79,8 +78,6 @@ export class DemoPageComponent implements OnInit {
   }
 
 }
-
-
 
 function mockProfileMenu(): Person {
   const avatarProps = {
@@ -118,12 +115,12 @@ function mockDropdown(): any {
   const dropdownInputs = {
     items: ['item1', 'item2 which is longer', 'item3 which is longer and ', 'item4', 'item5', 'item6', 'item7'],
     itemsNumber: ['1', '2', '3', '4', '5', '11', '22', '33', '44', '55'],
-    textTrimNumber: 2,
-    selectedItemDefault: 'Page',
+    textTrimNumber: 1,
+    selectedItemDefault: '',
     menuExtensionDir: MenuExtensionDirection.ToRight,
     dropDownFieldType: DropdownFieldType.Input,
-    fieldStatusType: FieldStatusType.Help,
-    isDisable: false,
+    fieldStatusType: FieldStatusType.Error,
+    isDisable: true,
   };
   return dropdownInputs;
 }

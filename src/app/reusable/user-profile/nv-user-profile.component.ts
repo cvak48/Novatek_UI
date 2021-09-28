c737b4719afcae09187099750c42a8c745bfd42e
Sina-pz <sp.zanoosi@gmail.com>
2021-09-28 09:58:00 -0400

    new image not working

--- src/app/reusable/user-profile/nv-user-profile.component.ts
+++ src/app/reusable/user-profile/nv-user-profile.component.ts
@@ -1,47 +1,53 @@
 import { UserProfileService } from './../../services/local-data/user-profile/user-profile.service';
 import { Person, Notification, User } from './../../model/data-model';
-import { Component, Input, OnInit } from '@angular/core';
-
-
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-nv-user-profile',
   templateUrl: './nv-user-profile.component.html',
-  styleUrls: ['./nv-user-profile.component.scss']
+  styleUrls: ['./nv-user-profile.component.scss'],
 })
 export class NvUserProfileComponent implements OnInit {
   // Avatar
   @Input() person!: Person;
   userProfiles!: User[];
-  constructor(private _dataService: UserProfileService) { }
+  @ViewChild('fileDropRef') fileDropRef!: ElementRef<HTMLElement>;
+  constructor(private _dataService: UserProfileService) {}
 
   ngOnInit(): void {
     this._initialize();
-
   }
-  onIconClick(): void {
-
-  }
-    /**
-   * event handler; receive file as the user choose it from local folder
+  /**
+   * The img element and input is internally linked with onIconClick event handler;
+   * because the input style got hidden and we use menu svg icon instead
    */
-     fileBrowseHandler(event: any): void {
-      console.log((event?.target?.files));
-          }
+  onIconClick(): void {
+    // click on input
+    this.fileDropRef.nativeElement.click();
+  }
+  /**
+   * event handler; receive just image as the user choose it from local folder
+   */
+  fileBrowseHandler(event: any): void {
+    const files = event?.target?.files;
+    console.log(files);
+    this.person.imageUrl = files[0].name;
+  }
   private _initialize(): void {
     this.person = mockProfileMenu();
-    this._dataService.userProfileData$.subscribe(users =>
-      this.userProfiles = users
+    this._dataService.userProfileData$.subscribe(
+      (users) => (this.userProfiles = users)
     );
   }
 }
-
 
 function mockProfileMenu(): Person {
   const avatarProps = {
     id: 1,
     name: 'Alex Green',
-    imageUrl: 'https://tse1.mm.bing.net/th?id=OIP.E45HCyveqDL44p8lmvQL9AAAAA&pid=Api&P=0',
+    imageUrl:
+      'https://tse1.mm.bing.net/th?id=OIP.E45HCyveqDL44p8lmvQL9AAAAA&pid=Api&P=0',
     notification: {
       number: 200,
       content: '',

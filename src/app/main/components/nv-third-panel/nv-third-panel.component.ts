cbe7ee57f15e33e8351e1576004bc2dccb217746
veeranjaneyuludavuluri <veeranji826@gmail.com>
2021-10-12 01:41:25 -0400

    Added thrid panel

---
+++ src/app/main/components/nv-third-panel/nv-third-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nv-third-panel',
+  templateUrl: './nv-third-panel.component.html',
+  styleUrls: ['./nv-third-panel.component.scss']
+})
+export class NvThirdPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

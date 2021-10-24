39b9326506ff1b83a20e5c643a361ae958688380
Dhaval Tripathi <dhavaltripathi29@gmail.com>
2021-10-24 15:47:10 +0530

    data-pass-to-second-panel - updated data saved

--- src/app/services/application.service.ts
+++ src/app/services/application.service.ts
@@ -5,9 +5,14 @@ import { of } from 'rxjs';
 @Injectable({providedIn: 'root'})
 export class ApplicationService {
     selectedUserData: BehaviorSubject<any> = new BehaviorSubject({});
+    updatedUserData: BehaviorSubject<any> = new BehaviorSubject({});
     constructor() {}
 
     setSelectedUserData(data: any): void{
         this.selectedUserData.next(data);
+    }
+
+    setUpdatedUserData(data: any): void{
+        this.updatedUserData.next(data);
     }
 }

a5ea1e657b2d338f522df88d109b135f842e75c2
Dhaval Tripathi <dhavaltripathi29@gmail.com>
2021-11-10 16:04:02 +0530

    fourth panel show hide adjusted

--- src/app/main/components/nv-user-list-view/nv-user-list-view.component.ts
+++ src/app/main/components/nv-user-list-view/nv-user-list-view.component.ts
@@ -17,6 +17,10 @@ export class NvUserListViewComponent implements OnInit {
   columns: string[] = ['name', 'date', 'email'];
   sub = new Subscription();
   title: string = '';
+  showPanel1: boolean = false;
+  showPanel2: boolean = false;
+  showPanel3: boolean = false;
+  showPanel4: boolean = false;
   constructor(private dataService: DataService,
               private applicationService: ApplicationService) { }
 
@@ -29,22 +33,36 @@ export class NvUserListViewComponent implements OnInit {
            .subscribe(res => {
               this.title = (res == 'new' ? 'New User' : 'Edit User') ;
            });
+    this.showPanel1 = true;
   }
 
   panelClicked(panel: string): void{
     switch(panel){
       case '1':
         this.showPanel='2';
+        this.showPanel1 = true;
+        this.showPanel2 = true;
         this.isLeftVisible = !this.isLeftVisible
         break;
       case '2':
         this.showPanel='3';
+        this.showPanel1 = true;
+        this.showPanel2 = true;
+        this.showPanel3 = true;
         break;
       case '3':
           this.showPanel='4';
+        this.showPanel1 = false;
+        this.showPanel2 = true;
+        this.showPanel3 = true;
+        this.showPanel4 = true;
         break;
       case '4':
           this.showPanel='1';
+        this.showPanel1 = false;
+        this.showPanel2 = false;
+        this.showPanel3 = false;
+        this.showPanel4 = false;
         break;
     }
   }
@@ -54,15 +72,31 @@ export class NvUserListViewComponent implements OnInit {
       case '1':
         this.showPanel='0';
         this.isLeftVisible = !this.isLeftVisible
+        this.showPanel1 = false;
+          this.showPanel2 = false;
+          this.showPanel3 = false;
+          this.showPanel4 = false;
         break;
       case '2':
         this.showPanel='1';
+        this.showPanel1 = true;
+          this.showPanel2 = false;
+          this.showPanel3 = false;
+          this.showPanel4 = false;
         break;
       case '3':
           this.showPanel='2';
+          this.showPanel1 = true;
+          this.showPanel2 = true;
+          this.showPanel3 = false;
+          this.showPanel4 = false;
         break;
       case '4':
           this.showPanel='3';
+          this.showPanel1 = false;
+          this.showPanel2 = true;
+          this.showPanel3 = true;
+          this.showPanel4 = false;
         break;
     }
   }
